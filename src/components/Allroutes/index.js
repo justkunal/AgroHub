ffcd60c9d01ce9e2e4b61e33c194fb0e47f28821
Kunal <97599925+justkunal@users.noreply.github.com>
2024-02-11 19:58:06 +0530

    file upload

--- src/components/Allroutes/index.js
+++ src/components/Allroutes/index.js
@@ -10,21 +10,9 @@ import {
 import Dashboard from "../User-Dash/Dashboard/dashboard";
 import Dashbaord2 from "../User-Dash/Dashboard2/dashboard2";
 import CounsellorDashboard from "../Counsellor-Dash/Dash-counsellor/dashboard";
-import CounsellorSession from "../Counsellor-Dash/Session-Counsellor/session";
-import SessionClientDetail from "../Counsellor-Dash/Session-Clients-detail/session";
-import CounsellorManageCommunity from "../Counsellor-Dash/Counsellor-Manage-Community/community";
-import CounsellorCommunity from "../Counsellor-Dash/Counsellor-community/communuty";
-import CounsellorCommunityDiscussion from "../Counsellor-Dash/Counsellor-community-discussion/community";
 import Profile from "../User-Dash/Profile/profile";
 import ProfileSelfAssessment from "../User-Dash/Profile-self-assesment/profile";
-import Payment from "../User-Dash/Payment-History/payment";
-import Addpayment from "../User-Dash/Payment-add-details/paymentAdd";
-import Book from "../Book-Session/book";
-import Confirmation from "../Confirmation/confirmation";
-import Created from "../Created/created";
 import Profilecounsellor from "../Counsellor-Dash/Profile-counsellor/profile";
-import PaymentCo from "../Counsellor-Dash/Payment-Counsellor/payment";
-import PaymentAddC from "../Counsellor-Dash/Payment-add-details-counsellor/paymentAdd";
 import Login from "../Main/Registration/Login/Login";
 import Homecontent from "../Main/Home";
 import SignUp from "../Main/Registration/SignUp/SignUp";
@@ -32,10 +20,8 @@ import Aboutus from "../Main/Aboutus";
 import ContactUs from "../Main/Contactus";
 import Counsellors from "../Main/Counsellors/Counsellors";
 import { AnimatePresence } from "framer-motion";
-import Assessment from "../Main/Assesment/assesment";
 import Cookies from "js-cookie";
 import axios from "axios";
-import Dashboard2 from "../User-Dash/Dashboard2/dashboard2";
 
 export default function Allroutes() {
   const location = useLocation();
@@ -137,37 +123,14 @@ export default function Allroutes() {
         />
 
         <Route path="/dashboard-counsellor" element={<CounsellorDashboard />} />
-        <Route path="/session-counsellor" element={<CounsellorSession />} />
-        <Route
-          path="/session-client-details"
-          element={<SessionClientDetail />}
-        />
-        <Route
-          path="/counsellor-manage-community"
-          element={<CounsellorManageCommunity />}
-        />
-        <Route path="/counsellor-community" element={<CounsellorCommunity />} />
-        <Route
-          path="/counsellor-community-discussion"
-          element={<CounsellorCommunityDiscussion />}
-        />
+
         <Route path="/profile" element={<Profile />} />
         <Route
           path="/profile-self-assessment"
           element={<ProfileSelfAssessment />}
         />
-        <Route path="/payment-history" element={<Payment />} />
-        <Route path="/add-payment-details" element={<Addpayment />} />
-        <Route path="/book-session" element={<Book />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-        <Route path="/created" element={<Created />} />
+
         <Route path="/profile-counsellor" element={<Profilecounsellor />} />
-        <Route path="/payment-counsellor" element={<PaymentCo />} />
-        <Route path="/assessment" element={<Assessment />} />
-        <Route
-          path="/add-payment-details-counsellor"
-          element={<PaymentAddC />}
-        />
       </Routes>
     </AnimatePresence>
   );
